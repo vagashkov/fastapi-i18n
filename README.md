95b493d902d223f689f36f2baffefaccf007a370
vagashkov <vitali.agashkov@gmail.com>
2022-11-30 12:43:09 +0300

    Basic version with desired locale as a parameter

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fastapi-i18n
-Simple JSON-files based internationalization sample
+Simple JSON-based internationalization sample
+
+
+To view demo:
+- clone this repo
+- create fresh venv and activate it
+- install packages according to requirements.txt file
+- navigate to fastapi-i18n directory
+- launch application with 'uvicorn src.apart_app:app' command
+- open http://127.0.0.1:8000/apartment/en to view page in English
+- open http://127.0.0.1:8000/apartment/de to view page in German
+
---
+++ src/apart_app.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from babel.plural import PluralRule
+import glob
+import json
+import os
+from pathlib import Path
+
+# define project components location
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# create an application
+app = FastAPI()
+# defile templates location
+templates = Jinja2Templates(directory=str(BASE_DIR.joinpath('templates')))
+
+# default language if requested is not found
+default_fallback = 'en'
+# languages storage
+languages = {}
+# rule to distinguish single from plurals
+plural_rule = PluralRule({'one': 'n in 0..1'})
+
+# load language files
+languages_dir = str(BASE_DIR.joinpath('languages'))
+language_list = glob.glob("{}/*.json".format(languages_dir))
+
+for lang in language_list:
+    # extract language code from filename
+    filename = lang.split(os.sep)
+    lang_code = filename[-1].split('.')[0]
+
+    # and load it into languages storage
+    with open(lang, 'r', encoding='utf8') as file:
+        languages[lang_code] = json.load(file)
+
+
+# create custom filters for Jinja2 and plural nouns
+def plural_formatting(key_value, input_text, locale):
+    # looking for label in selected language
+    key = ''
+    for i in languages[locale]:
+        if key_value == languages[locale][i]:
+            key = i
+            break
+    # label not found
+    if not key:
+        return key_value
+
+    # build key name for plural value labed (bedroom -> bedroom_plural)
+    plural_key = "{}_plural".format(key)
+
+    # if plural needed and found in language file - use it
+    if plural_rule(input_text) != 'one' and plural_key in languages[locale]:
+        key = plural_key
+
+    return languages[locale][key]
+
+
+# assign plurals filter to Jinja2
+templates.env.filters['plural_formatting'] = plural_formatting
+
+
+@app.get("/apartment/{locale}", response_class=HTMLResponse)
+async def rental(request: Request, locale: str):
+    # if language is unknown - use en
+    if locale not in languages:
+        locale = default_fallback
+
+    # bild response data
+    result = {"request": request}
+    # update it with translations
+    result.update(languages[locale])
+    # and make some data changes (to check if plurals rule is working)
+    result.update({'locale': locale, 'bedroom_value': 2})
+
+    # and finally return template-based response
+    return templates.TemplateResponse("index.html", result)
